7eeab4902dc08a1aa2f792ed0fdb23dceb1a7025
alvcodec <148978672+Alvicodes@users.noreply.github.com>
2025-06-29 18:03:22 +0100

    header and delivery section update

--- Restaurant Website/main.js
+++ Restaurant Website/main.js
@@ -13,3 +13,34 @@ navLinks.addEventListener("click", (e) => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header_image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".header_tag", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header_content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".header_content .section_description", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".header_btns", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
